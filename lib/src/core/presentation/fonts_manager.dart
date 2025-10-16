7ba41c1c262c59fcc3fe38ee33dda39824823187
bakrbasser <zedalbser123@gmail.com>
2025-10-16 04:23:20 +0300

    added fonts manager

---
+++ lib/src/core/presentation/fonts_manager.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+TextStyle IBMLight({Color color = Colors.black, double fontSize = 16}) {
+  return TextStyle(
+    fontFamily: 'IBM',
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.w300,
+  );
+}
+
+TextStyle IBMRegular({Color color = Colors.black, double fontSize = 16}) {
+  return TextStyle(
+    fontFamily: 'IBM',
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+TextStyle IBMMedium({Color color = Colors.black, double fontSize = 16}) {
+  return TextStyle(
+    fontFamily: 'IBM',
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.w500,
+  );
+}
+
+TextStyle IBMBold({Color color = Colors.black, double fontSize = 16}) {
+  return TextStyle(
+    fontFamily: 'IBM',
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.w700,
+  );
+}
